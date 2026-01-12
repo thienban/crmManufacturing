c386a78b05912e1ad9293372d560567669b8c407
Thien Bui <thienban91@hotmail.fr>
2026-01-12 02:27:47 +0100

    remove default url

--- src/trpc/Provider.tsx
+++ src/trpc/Provider.tsx
@@ -23,7 +23,7 @@ export function TRPCProvider({ children }: { children: React.ReactNode }) {
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    url: `${process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'}/api/trpc`,
+                    url: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`,
                     transformer: SuperJSON,
                 }),
             ],
